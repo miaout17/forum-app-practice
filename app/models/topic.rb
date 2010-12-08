01ef01319cb657f7ea091a8d55af08e56f7f345c
BigCat <yc_ling@htc.com>
2010-12-08 17:51:51 +0800

    sort by reverse order defaultly

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -17,5 +17,7 @@ class Topic < ActiveRecord::Base
   belongs_to :board
   has_many :posts, :inverse_of => :topic
 
+  default_scope :order => 'id DESC'
+
   validates_presence_of :title, :board_id
 end
