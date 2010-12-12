f92a653ab79b74739c62c5dd5e49d166259e0bdb
BigCat <yc_ling@htc.com>
2010-12-12 21:10:16 +0800

    add user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 
 describe UsersController do
 
+  it "#find_user" do
+    @user = mock_model(User)
+    controller.params = {:id => @user.id}
+
+    User.should_receive(:find).with(@user.id).and_return(@user)
+    controller.send(:find_user)
+
+    assigns(:user).should eq(@user)
+  end
+
+  def should_find_user
+    @user = mock_model(User)
+    controller.should_receive(:find_user) { controller.instance_variable_set("@user", @user) }.ordered
+  end
+
+  describe "GET show" do
+    it "gets the user by id" do
+      should_find_user
+    end
+  end
+
 end
