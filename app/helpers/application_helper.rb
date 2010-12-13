0712df05fd4c6da47a97b0c0b8366163a507c983
BigCat <yc_ling@htc.com>
2010-12-13 14:16:26 +0800

    use rails.png as default personal icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,18 @@ module ApplicationHelper
   end
 
   def user_icon_tag(user, style = :icon)
-    return "" if user.nil?
-    return "" unless user.icon.exists?
+    return default_user_icon_tag(style) unless user.icon.exists?
     return image_tag(user.icon.url(style))
+  end
+
+  private
+  def default_user_icon_tag(style)
+    # TODO: move the magic number to somewhere else
+    # Give another default image rather than rails.png?
+    size = case style
+      when :icon then 32
+      when :thumb then 100
+    end
+    return image_tag("/images/rails.png", :width => size, :height => size)
   end
 end
