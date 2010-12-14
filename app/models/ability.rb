3946740144e51c6b67d003e6d99f3957669f92a6
BigCat <yc_ling@htc.com>
2010-12-14 17:52:14 +0800

    fixes nill user problem

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    user ||= User.new # guest user
     can :update, Post, :user_id => user.id
+
     # if user.admin?
     #   can :manage, :all
     # else
