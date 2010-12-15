19932ac8b2d14ac84ea330b6dba4505ecf158faa
BigCat <yc_ling@htc.com>
2010-12-15 14:51:06 +0800

    category parent selector ok

--- app/helpers/admin/categories_helper.rb
+++ app/helpers/admin/categories_helper.rb
@@ -1,2 +1,7 @@
 module Admin::CategoriesHelper
+  def select_parent_tag(category)
+    valid_parents = category.valid_parents
+    options = options_from_collection_for_select(valid_parents, :id, :name)
+    select(:category, :parent_id, options)
+  end
 end
