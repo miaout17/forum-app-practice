b922a7586b0c0884f74b66dab709a8faf9f66c87
BigCat <yc_ling@htc.com>
2010-12-15 12:32:35 +0800

    ...

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    raise
 
     user ||= User.new # guest user
     can :update, Post, :user_id => user.id
--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Admin::CategoriesController do
+  include ApplicationSpecHelperMethods
+
+  before(:each) do
+    should_authenticate_user
+    @current_user.stub!(:admin?).and_return(true) #TODO: manager
+  end
+
+  def should_load_resource(*resources)
+  end
+
+  def should_authorize_resource
+    controller.should_receive(:authorize_resource!) do
+      raise
+    end
+  end
+
+  describe "GET index" do
+    it "hello" do
+      should_load_resource(:categories)
+      should_authorize_resource
+
+      get :index
+    end
+  end
 
 end
+
